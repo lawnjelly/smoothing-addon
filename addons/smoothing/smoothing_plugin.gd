6f80b70d6e208625bfb5253c9e90e2c15e673e6b
Niclas Eriksen <niclaseriksen@gmx.com>
2022-04-26 15:44:52 +0200

    Godot 4.0 update on 2D and main GD file.

--- addons/smoothing/smoothing_plugin.gd
+++ addons/smoothing/smoothing_plugin.gd
@@ -1,11 +1,11 @@
-tool
+@tool
 extends EditorPlugin
 
 
 func _enter_tree():
 	# Initialization of the plugin goes here
 	# Add the new type with a name, a parent type, a script and an icon
-	add_custom_type("Smoothing", "Spatial", preload("smoothing.gd"), preload("smoothing.png"))
+	add_custom_type("Smoothing", "Node3D", preload("smoothing.gd"), preload("smoothing.png"))
 	add_custom_type("Smoothing2D", "Node2D", preload("smoothing_2d.gd"), preload("smoothing_2d.png"))
 	pass
 
