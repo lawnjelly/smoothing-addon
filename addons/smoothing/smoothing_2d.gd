2d4cc2959a4927e35799135fb7497679b1748b4f
lawnjelly <lawnjelly@gmail.com>
2023-03-07 09:20:26 +0000

    Default 2D to use global mode
    Local mode can be confusing for some users, default is now foolproof, at the cost of maybe higher processing.

--- addons/smoothing/smoothing_2d.gd
+++ addons/smoothing/smoothing_2d.gd
@@ -50,7 +50,7 @@ const SF_GLOBAL_OUT = 1 << 5
 const SF_INVISIBLE = 1 << 6
 
 
-@export_flags("enabled", "translate", "rotate", "scale", "global in", "global out") var flags: int = SF_ENABLED | SF_TRANSLATE:
+@export_flags("enabled", "translate", "rotate", "scale", "global in", "global out") var flags: int = SF_ENABLED | SF_TRANSLATE | SF_ROTATE | SF_SCALE | SF_GLOBAL_IN | SF_GLOBAL_OUT:
 	set(v):
 		flags = v
 		# we may have enabled or disabled
@@ -96,7 +96,6 @@ func _ready():
 	m_Angle_curr = 0
 	m_Angle_prev = 0
 	set_process_priority(100)
-	set_as_top_level(true)
 	Engine.set_physics_jitter_fix(0.0)
 
 
@@ -108,6 +107,7 @@ func _SetProcessing():
 	set_process(bEnable)
 	set_physics_process(bEnable)
 
+	set_as_top_level(_TestFlags(SF_GLOBAL_OUT))
 
 func _enter_tree():
 	# might have been moved
@@ -193,30 +193,21 @@ func _process(_delta):
 
 	var f = Engine.get_physics_interpolation_fraction()
 
-	if _TestFlags(SF_GLOBAL_OUT):
-		# translate
-		if _TestFlags(SF_TRANSLATE):
-			set_global_position(m_Pos_prev.lerp(m_Pos_curr, f))
+	# We can always use local position rather than set_global_position
+	# because even in global mode we are set_as_top_level, and the result
+	# will be the same.
 
-		# rotate
-		if _TestFlags(SF_ROTATE):
-			var r = _LerpAngle(m_Angle_prev, m_Angle_curr, f)
-			set_global_rotation(r)
+	# translate
+	if _TestFlags(SF_TRANSLATE):
+		set_position(m_Pos_prev.lerp(m_Pos_curr, f))
 
-		if _TestFlags(SF_SCALE):
-			set_global_scale(m_Scale_prev.lerp(m_Scale_curr, f))
-	else:
-		# translate
-		if _TestFlags(SF_TRANSLATE):
-			set_position(m_Pos_prev.lerp(m_Pos_curr, f))
+	# rotate
+	if _TestFlags(SF_ROTATE):
+		var r = _LerpAngle(m_Angle_prev, m_Angle_curr, f)
+		set_rotation(r)
 
-		# rotate
-		if _TestFlags(SF_ROTATE):
-			var r = _LerpAngle(m_Angle_prev, m_Angle_curr, f)
-			set_rotation(r)
-
-		if _TestFlags(SF_SCALE):
-			set_scale(m_Scale_prev.lerp(m_Scale_curr, f))
+	if _TestFlags(SF_SCALE):
+		set_scale(m_Scale_prev.lerp(m_Scale_curr, f))
 
 	pass
 
